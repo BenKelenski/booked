2aa5a5e7c3bddef621bc24cf7d053df1cebe49cd
BenKelenski <ben.kelenski@gmail.com>
2025-01-27 21:55:51 -0500

    added service layer for users

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
 
 from app.models.user import User, UserCreate, UserPublic
+from app.services.user import UserSerivce
 from app.dependencies import SessionDep
 
 router = APIRouter(
@@ -9,25 +11,32 @@ router = APIRouter(
     tags=["users"],
 )
 
+def get_user_serivce(session: SessionDep) -> UserSerivce:
+    return UserSerivce(session)
 
 @router.get("/", response_model=list[UserPublic])
-async def get_all_users(session: SessionDep) -> list[UserPublic]:
-    users = session.exec(select(User)).all()
-    return users
+async def get_all_users(userService: Annotated[UserSerivce, Depends(get_user_serivce)]) -> list[UserPublic]:
+    return userService.get_all_users()
 
 
 @router.get("/{user_id}", response_model=UserPublic)
-async def get_user(user_id: int, session: SessionDep):
-    user = session.exec(select(User).where(User.id == user_id)).first()
+async def get_user(user_id: int, userService: Annotated[UserSerivce, Depends(get_user_serivce)]) -> UserPublic:
+    user = userService.get_user(user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
 @router.post("/", response_model=UserPublic)
-async def create_user(user_request: UserCreate, session: SessionDep):
-    user = User.model_validate(user_request)
-    session.add(user)
-    session.commit()
-    session.refresh(user)
+async def create_user(user_request: UserCreate, userService: Annotated[UserSerivce, Depends(get_user_serivce)]) -> UserPublic:
+    user = userService.create_user(user_request)
     return user
+
+@router.delete("/{user_id}")
+async def delete_user(user_id: int, session: SessionDep):
+    user = session.exec(select(User).where(User.id == user_id)).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"message": "User deleted successfully"}
---
+++ app/services/user.py
@@ -0,0 +1,31 @@
+from sqlmodel import select
+from app.dependencies import SessionDep
+from app.models.user import User, UserCreate, UserPublic
+
+
+class UserSerivce:
+
+    def __init__(self, session: SessionDep):
+        self.session = session
+
+    def get_all_users(self) -> list[UserPublic]:
+        return self.session.exec(select(User)).all()
+
+    def get_user(self, user_id: int) -> UserPublic | None:
+        user = self.session.exec(select(User).where(User.id == user_id)).first()
+        return user
+    
+    def create_user(self, user_create: UserCreate) -> UserPublic:
+        user = User.model_validate(user_create)
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+    
+    def delete_user(self, user_id: int):
+        user = self.session.exec(select(User).where(User.id == user_id)).first()
+        if user is None:
+            return None
+        self.session.delete(user)
+        self.session.commit()
+        return {"message": "User deleted successfully"}
