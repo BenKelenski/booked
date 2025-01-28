0c5288d5b4e1dd985a13c840fb0bf8793ed870ea
BenKelenski <ben.kelenski@gmail.com>
2025-01-27 22:00:05 -0500

    change file name and call delete function

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
 
 from app.models.user import User, UserCreate, UserPublic
-from app.services.user import UserSerivce
+from app.services.users import UserSerivce
 from app.dependencies import SessionDep
 
 router = APIRouter(
@@ -33,10 +33,5 @@ async def create_user(user_request: UserCreate, userService: Annotated[UserSeriv
     return user
 
 @router.delete("/{user_id}")
-async def delete_user(user_id: int, session: SessionDep):
-    user = session.exec(select(User).where(User.id == user_id)).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    session.delete(user)
-    session.commit()
-    return {"message": "User deleted successfully"}
+async def delete_user(user_id: int, userService: Annotated[UserSerivce, Depends(get_user_serivce)]):
+    return userService.delete_user(user_id)
