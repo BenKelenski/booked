220b60fb74bf4c4585389fff1e67e3232d3ab197
BenKelenski <ben.kelenski@gmail.com>
2025-01-28 21:37:16 -0500

    added books service and router changes

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException
-from sqlmodel import select
+from typing import Annotated
+from fastapi import APIRouter, Depends
 
-from app.models.book import Book, BookCreate, BookPublic
+from app.models.book import BookCreate, BookPublic
 from app.dependencies import SessionDep
+from app.services.books import BookService
 
 router = APIRouter(
     prefix="/books",
@@ -10,24 +11,36 @@ router = APIRouter(
 )
 
 
+def get_book_service(session: SessionDep):
+    return BookService(session)
+
+
 @router.get("/")
-async def get_all_books(session: SessionDep) -> list[BookPublic]:
-    return session.exec(select(Book)).all()
+async def get_all_books(
+    bookService: Annotated[BookService, Depends(get_book_service)],
+) -> list[BookPublic]:
+    return bookService.get_all_books()
 
 
 @router.get("/{book_id}")
-async def get_book(book_id: int, session: SessionDep) -> BookPublic:
-    book = session.exec(select(Book).where(Book.id == book_id)).first()
-    if book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-
-    return book
+async def get_book(
+    book_id: int, bookService: Annotated[BookService, Depends(get_book_service)]
+) -> BookPublic:
+    return bookService.get_book(book_id)
 
 
 @router.post("/")
-async def create_book(book_request: BookCreate, session: SessionDep) -> BookPublic:
-    book = Book.model_validate(book_request)
-    session.add(book)
-    session.commit()
-    session.refresh(book)
-    return book
+async def create_book(
+    book_request: BookCreate,
+    bookService: Annotated[BookService, Depends(get_book_service)],
+) -> BookPublic:
+    return bookService.create_book(book_request)
+
+
+@router.delete(
+    "/{book_id}",
+)
+async def delete_book(
+    book_id: int, bookService: Annotated[BookService, Depends(get_book_service)]
+) -> dict[str, str]:
+    return bookService.delete_book(book_id)
---
+++ app/services/books.py
@@ -0,0 +1,33 @@
+from fastapi import HTTPException
+from sqlmodel import select
+from app.dependencies import SessionDep
+from app.models.book import Book, BookCreate, BookPublic
+
+
+class BookService:
+    def __init__(self, session: SessionDep):
+        self.session = session
+
+    def get_all_books(self) -> list[BookPublic]:
+        return self.session.exec(select(Book)).all()
+
+    def get_book(self, book_id: int) -> BookPublic | None:
+        book = self.session.exec(select(Book).where(Book.id == book_id)).first()
+        if book is None:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return book
+
+    def create_book(self, book_request: BookCreate) -> BookPublic:
+        book = Book.model_validate(book_request)
+        self.session.add(book)
+        self.session.commit()
+        self.session.refresh(book)
+        return book
+
+    def delete_book(self, book_id: int) -> dict[str, str]:
+        book = self.session.exec(select(Book).where(Book.id == book_id)).first()
+        if book is None:
+            return {"message": "Book not found"}
+        self.session.delete(book)
+        self.session.commit()
+        return {"message": "Book deleted successfully"}
