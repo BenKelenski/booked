9e677450e3ee6a4ab24eae12a8d76e0eb6075ad6
BenKelenski <ben.kelenski@gmail.com>
2025-01-28 21:36:06 -0500

    users route/service changes

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from app.models.user import UserCreate, UserPublic
 from app.services.users import UserSerivce
@@ -26,10 +26,7 @@ async def get_all_users(
 async def get_user(
     user_id: int, userService: Annotated[UserSerivce, Depends(get_user_serivce)]
 ) -> UserPublic:
-    user = userService.get_user(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    return userService.get_user(user_id)
 
 
 @router.post("/", response_model=UserPublic)
@@ -37,12 +34,11 @@ async def create_user(
     user_request: UserCreate,
     userService: Annotated[UserSerivce, Depends(get_user_serivce)],
 ) -> UserPublic:
-    user = userService.create_user(user_request)
-    return user
+    return userService.create_user(user_request)
 
 
 @router.delete("/{user_id}")
 async def delete_user(
     user_id: int, userService: Annotated[UserSerivce, Depends(get_user_serivce)]
-):
+) -> dict[str, str]:
     return userService.delete_user(user_id)
--- app/services/users.py
+++ app/services/users.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlmodel import select
 from app.dependencies import SessionDep
 from app.models.user import User, UserCreate, UserPublic
@@ -12,6 +13,8 @@ class UserSerivce:
 
     def get_user(self, user_id: int) -> UserPublic | None:
         user = self.session.exec(select(User).where(User.id == user_id)).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
         return user
 
     def create_user(self, user_create: UserCreate) -> UserPublic:
@@ -21,10 +24,10 @@ class UserSerivce:
         self.session.refresh(user)
         return user
 
-    def delete_user(self, user_id: int):
+    def delete_user(self, user_id: int) -> dict[str, str]:
         user = self.session.exec(select(User).where(User.id == user_id)).first()
         if user is None:
-            return None
+            return {"message": "User not found"}
         self.session.delete(user)
         self.session.commit()
         return {"message": "User deleted successfully"}
