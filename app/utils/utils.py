33eb36254171dc786b4f9141760df309b42381b1
BenKelenski <ben.kelenski@gmail.com>
2025-01-29 22:01:33 -0500

    lint and format

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -5,8 +5,10 @@ from bcrypt import hashpw, gensalt, checkpw
 def get_timestamp_utc() -> datetime:
     return datetime.now(tz=timezone.utc).isoformat(timespec="milliseconds")
 
+
 def hash_and_salt_password(password: str) -> bytes:
     return hashpw(password.encode(), gensalt())
-    
+
+
 def verify_password(password: str, hashed_password: bytes) -> bool:
     return checkpw(password.encode(), hashed_password)
