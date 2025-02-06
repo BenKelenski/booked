78b9f7493c9a8c81ee4862a467007c0332add199
BenKelenski <ben.kelenski@gmail.com>
2025-02-05 22:00:23 -0500

    created collection repo

---
+++ app/repositories/collection_repo.py
@@ -0,0 +1,30 @@
+from sqlmodel import select
+from app.dependencies import SessionDep
+from app.models.collection import (
+    CollectionPublic,
+    Collection,
+    CollectionPublicWithBooks,
+)
+
+
+class CollectionRepository:
+    def __init__(self, session: SessionDep):
+        self.session = session
+
+    def get_all_collections(self) -> list[CollectionPublic]:
+        return self.session.exec(select(Collection)).all()
+
+    def get_collection(self, collection_id) -> CollectionPublicWithBooks:
+        return self.session.exec(
+            select(Collection).where(Collection.id == collection_id)
+        ).first()
+
+    def create_collection(self, collection: Collection) -> CollectionPublic:
+        self.session.add(collection)
+        self.session.commit()
+        self.session.refresh(collection)
+        return collection
+
+    def delete_collection(self, collection: Collection):
+        self.session.delete(collection)
+        self.session.commit()
