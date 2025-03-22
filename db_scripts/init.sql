783a2418c5c2398e52d66a674668f79c3a633203
BenKelenski <ben.kelenski@gmail.com>
2025-03-22 19:08:24 -0400

    removed explicit IDs from data

--- db_scripts/init.sql
+++ db_scripts/init.sql
@@ -14,12 +14,12 @@ CREATE TABLE users (
     is_admin BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO users (id, name, hashed_password, is_active, is_admin)
-VALUES  (1, 'Ben', convert_to('password', 'UTF-8'), TRUE, TRUE),
-        (2, 'Megan', convert_to('secretpass', 'UTF-8'), TRUE, TRUE),
-        (3, 'Chip', convert_to('treatsplz', 'UTF-8'), TRUE, TRUE),
-        (4, 'Libby', convert_to('iluvsqueakyball', 'UTF-8'), TRUE, TRUE),
-        (5, 'Lou Lou', convert_to('feedmehumans', 'UTF-8'), TRUE, TRUE);
+INSERT INTO users (name, hashed_password, is_active, is_admin)
+VALUES  ('Ben', convert_to('password', 'UTF-8'), TRUE, TRUE),
+        ('Megan', convert_to('secretpass', 'UTF-8'), TRUE, TRUE),
+        ('Chip', convert_to('treatsplz', 'UTF-8'), TRUE, TRUE),
+        ('Libby', convert_to('iluvsqueakyball', 'UTF-8'), TRUE, TRUE),
+        ('Lou Lou', convert_to('feedmehumans', 'UTF-8'), TRUE, TRUE);
 
 CREATE TABLE collections (
     id SERIAL PRIMARY KEY,
@@ -30,14 +30,14 @@ CREATE TABLE collections (
     user_id INT REFERENCES users(id)
 );
 
-INSERT INTO collections (id, name, description, user_id)
-VALUES  (1, 'Reading', 'Books I''m currently reading', 1),
-        (2, 'Want To Read', 'Collection of books I plan to read', 1),
-        (3, 'Read', 'Books I''ve already finished', 1),
-        (4, 'Megan''s Collection', 'This is Megan''s collection', 2),
-        (5, 'Chip''s Collection', 'This is Chip''s collection', 3),
-        (6, 'Libby''s Collection', 'This is Libby''s collection', 4),
-        (7, 'Lou Lou''s Collection', 'This is Lou Lou''s collection', 5);
+INSERT INTO collections (name, description, user_id)
+VALUES  ('Reading', 'Books I''m currently reading', 1),
+        ('Want To Read', 'Collection of books I plan to read', 1),
+        ('Read', 'Books I''ve already finished', 1),
+        ('Megan''s Collection', 'This is Megan''s collection', 2),
+        ('Chip''s Collection', 'This is Chip''s collection', 3),
+        ('Libby''s Collection', 'This is Libby''s collection', 4),
+        ('Lou Lou''s Collection', 'This is Lou Lou''s collection', 5);
 
 UPDATE collections
 SET is_private = TRUE
@@ -52,16 +52,16 @@ CREATE TABLE books (
     collection_id INT REFERENCES collections(id)
 );
 
-INSERT INTO books (id, title, author, description, collection_id)
-VALUES  (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 'A classic novel about the American Dream', 1),
-        (2, 'The Catcher in the Rye', 'J.D. Salinger', 'A novel about teenage angst', 1),
-        (3, 'The Bell Jar', 'Sylvia Plath', 'A novel about a young', 2),
-        (4, 'The Road', 'Cormac McCarthy', 'A novel about a father and son', 2),
-        (5, 'The Hobbit', 'J.R.R. Tolkien', 'A novel about a hobbit', 3),
-        (6, 'The Lord of the Rings', 'J.R.R. Tolkien', 'A novel about a ring', 3),
-        (7, 'The Hunger Games', 'Suzanne Collins', 'A novel about a dystopian future', 4),
-        (8, 'Catching Fire', 'Suzanne Collins', 'A novel about a dystopian future', 4),
-        (9, 'Mockingjay', 'Suzanne Collins', 'A novel about a dystopian future', 4),
-        (10, 'The Giver', 'Lois Lowry', 'A novel about a dystopian future', 5),
-        (11, 'Gathering Blue', 'Lois Lowry', 'A novel about a dystopian future', 5),
-        (12, 'Messenger', 'Lois Lowry', 'A novel about a dystopian future', 5);
+INSERT INTO books (title, author, description, collection_id)
+VALUES  ('The Great Gatsby', 'F. Scott Fitzgerald', 'A classic novel about the American Dream', 1),
+        ('The Catcher in the Rye', 'J.D. Salinger', 'A novel about teenage angst', 1),
+        ('The Bell Jar', 'Sylvia Plath', 'A novel about a young', 2),
+        ('The Road', 'Cormac McCarthy', 'A novel about a father and son', 2),
+        ('The Hobbit', 'J.R.R. Tolkien', 'A novel about a hobbit', 3),
+        ('The Lord of the Rings', 'J.R.R. Tolkien', 'A novel about a ring', 3),
+        ('The Hunger Games', 'Suzanne Collins', 'A novel about a dystopian future', 4),
+        ('Catching Fire', 'Suzanne Collins', 'A novel about a dystopian future', 4),
+        ('Mockingjay', 'Suzanne Collins', 'A novel about a dystopian future', 4),
+        ('The Giver', 'Lois Lowry', 'A novel about a dystopian future', 5),
+        ('Gathering Blue', 'Lois Lowry', 'A novel about a dystopian future', 5),
+        ('Messenger', 'Lois Lowry', 'A novel about a dystopian future', 5);
